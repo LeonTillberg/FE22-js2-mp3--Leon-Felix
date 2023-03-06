6c6a55a0d49d1ee4458da7679605eedbd4c07ea8
LeonTillberg <leontm@live.se>
2023-03-06 20:39:09 +0100

    Skapat fungerande kakor som sparar produkters värden (namn, mängd, tot-pris) samt skapat kort för varje produkt som sen ska fyllas genom firebase

---
+++ main.js
@@ -0,0 +1,43 @@
+//Plain JS Cookie:
+function setCookie(name, value, expires) {
+    document.cookie = `${name}=${value};expires=${expires.toUTCString()}`;
+}
+function getCookie(name) {
+    const cookieValue = document.cookie.match(`(^|;)\\s*${name}\\s*=\\s*([^;]+)`);
+    return cookieValue ? cookieValue.pop() : '';
+}
+// När man trycker add to cart så skapas en placeholder av ditt angivna amount:
+function handleInput(event) {
+    const input = event.target;
+    const latestValue = input.value;
+    input.placeholder = latestValue;
+}
+// Skapar kakor för varje produkt som innehåller namn och amount:
+function handleSubmit(event) {
+    event.preventDefault();
+    // Kakans timer på 3min och totala priset för angiven frukt/mängd:
+    const expires = new Date(Date.now() + 3 * 60 * 1000);
+    const productPrice = parseFloat(event.target.parentNode.querySelector('p').innerText.replace(/[^\d.]/g, ''));
+    const totalProductPrice = productPrice * parseFloat(event.target.previousElementSibling.value);
+
+    // Set:er kakan med angiven mängd och pris*mängds värde:
+    setCookie(`${event.target.dataset.product}`, `${event.target.previousElementSibling.value}:${totalProductPrice}`, expires);
+
+    // Tömmer input fältet efter man tryckt på knappen:
+    event.target.previousElementSibling.value = "";
+}
+// När man trycker så skapas en kaka med värdet:
+const addToCartBtns = document.querySelectorAll('[data-product]');
+addToCartBtns.forEach(function (button) {
+    button.addEventListener('click', handleSubmit);
+});
+// När man trycker tas man till kundvagn:
+const doneShoppingBtn = document.querySelector('button');
+doneShoppingBtn.addEventListener('click', function () {
+    window.location.href = './kundvagn.html';
+});
+// När man trycker add to cart så skapas en placeholder av ditt angivna amount:
+const inputFields = document.querySelectorAll('input[type="number"]');
+inputFields.forEach(function (input) {
+    input.addEventListener('input', handleInput);
+});
