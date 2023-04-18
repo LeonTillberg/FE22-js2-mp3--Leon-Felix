8b48a4b0d1470c4f44d5757046a80aafd3ab8b9f
Leon Tillberg <112480830+LeonTillberg@users.noreply.github.com>
2023-04-18 10:02:36 +0200

    re-installed js-cookie

---
+++ modules/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAqmiEe8TtSYZoHkkoY1X5ReoGH01sl2QU",
+    authDomain: "storesite-e8d7c.firebaseapp.com",
+    databaseURL: "https://storesite-e8d7c-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "storesite-e8d7c",
+    storageBucket: "storesite-e8d7c.appspot.com",
+    messagingSenderId: "886798905451",
+    appId: "1:886798905451:web:8adb49015e1acbc770012e",
+    measurementId: "G-2WWX90KVMK"
+  };
